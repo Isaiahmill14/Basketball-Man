de5c9b883f3a471b4dc2b7ea121ab4765b21f2df
Isaiahmill14 <isaiahxmiller@gmail.com>
2024-02-28 09:32:18 -0500

    Fixed issue with image accepting styling changes based off wrong guesses and added end game functionality

--- js/main.js
+++ js/main.js
@@ -8,25 +8,39 @@ const maxGuesses = 10
 
 /*----- state variables -----*/
 let winner // guessed correctly/incorrectly
-let wrongGuesses = 0  // base value of guesses beginning at 0
+let wrongGuesses // container for wrong guesses
+let correctLetters  //container for correct guesses
 
 /*----- cached elements  -----*/
 const keyboardEl = document.querySelector('.keyboard-selector')
-const playAgainBtn = document.querySelector('.play-again')
 const basketballManImageEl = document.querySelector('.basketballman-box img')
 const wordDisplayEl = document.querySelector('.word-display')
 const guessesEl = document.querySelector('.guesses-text b')
 const randomWordEl = wordChoices[Math.floor(Math.random() * wordChoices.length)]
-console.log(keyboardEl)
+const gameMenu = document.querySelector('.game-menu')
+const playAgainBtn = document.querySelector('.play-again')
 
 /*----- event listeners -----*/
-playAgainBtn.addEventListener('click', init)
+
+/*----- initial game state -----*/
+basketballManImageEl.style.opacity = 1
 
 /*----- functions -----*/
 init()
 
 function init() {
     render()
+    
+}
+
+function gameOver(isVictory) {
+    setTimeout(() => {
+        //adjust post-game menu to reflect current game info
+        const gameMenuText = isVictory ? `You guessed the word: ` : `The correct word was: `
+        gameMenu.querySelector('h4').innerText = `${isVictory ? 'You Win!' : 'Game Over'}`
+        gameMenu.querySelector('p').innerHTML = `${gameMenuText} <b>${randomWordEl}</b>`
+        gameMenu.classList.add('show')
+    }, 300)
 }
 
 function initGame(button, clickedLetter) {
@@ -35,6 +49,7 @@ function initGame(button, clickedLetter) {
         //show correct letters on the word display
         [...randomWordEl].forEach((letter, idx) => {
             if (letter === clickedLetter) {
+                correctLetters.push(letter)
                 wordDisplayEl.querySelectorAll('li')[idx].innerText = letter
                 wordDisplayEl.querySelectorAll('li')[idx].classList.add('guessed')
             }
@@ -43,22 +58,39 @@ function initGame(button, clickedLetter) {
     } else {
         // update wrongGuesses when something is guessed incorrectly, dimenish img opacity .1 per wrong guess
         wrongGuesses++
-        // basketballManImageEl.style.opacity -= .1
+        console.log(parseFloat(basketballManImageEl.style.opacity))
+        basketballManImageEl.style.opacity = parseFloat(basketballManImageEl.style.opacity) - .1
         console.log(clickedLetter, 'does not exist in the word')
     }
-
+    
     button.disabled = true
     guessesEl.innerText = `${wrongGuesses} / ${maxGuesses}`
+    
+    // ends game if either of the conditions are met
+    if(wrongGuesses === maxGuesses) return gameOver(false)
+    if(correctLetters.length === randomWordEl.length) return gameOver(true)
 }
+
 
 function render() {
     renderRandomWord()
     renderKeyboard()
 }
 
-function renderRandomWord() {
+function resetGame() {
+    //resetting game and all variables
+    correctLetters = []
+    wrongGuesses = 0
+    guessesEl.innerText = `${wrongGuesses} / ${maxGuesses}`
+    wordDisplayEl.querySelectorAll('button').forEach((btn) => btn.disabled = false)
     wordDisplayEl.innerHTML = randomWordEl.split('').map(() => '<li class="letter"></li>').join('')
+    gameMenu.classList.remove('show')
+    
+}
+
+function renderRandomWord() {
     console.log(randomWordEl)
+    resetGame()
 }
 
 function renderKeyboard() {
@@ -70,6 +102,7 @@ function renderKeyboard() {
     }
 }
 
+playAgainBtn.addEventListener('click', renderRandomWord)
 
 
 /*----- unknown filing -----*/
